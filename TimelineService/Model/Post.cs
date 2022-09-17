55e0bc79564d996c260b48a87f9b4bb84c898fdc
Josse <josse@shellback.nl>
2022-09-17 13:23:41 +0200

    Add project files.

---
+++ TimelineService/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace TimelineService.Model
+{
+
+    public class Post
+    {
+        // ? is used to allow a value type to set to null
+        public int? Id { get; set; }
+        public string? Text { get; set; }
+        public string? UserId { get; set; }
+        public string? Likes { get; set; }
+    }
+}
---
+++ TimelineService/SQSProcessor/PostProcessor.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon;
+using Amazon.SQS.Model;
+
+namespace TimelineService.SQSProcessor
+{
+    public class PostProcessor : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Starting background processor");
+            var credentials = new BasicAWSCredentials("", "");
+            var client = new AmazonSQSClient(credentials, RegionEndpoint.EUCentral1);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"Getting messages from the queue {DateTime.Now}");
+                var request = new ReceiveMessageRequest()
+                {
+                    QueueUrl = "https://sqs.eu-central-1.amazonaws.com/075206908135/PostTimelineQueue",
+                    WaitTimeSeconds = 15,
+                    VisibilityTimeout = 20//for long polling
+
+                };
+                var response = await client.ReceiveMessageAsync(request);
+                foreach (var message in response.Messages)
+                {
+                    Console.WriteLine(message.Body);
+                    if (message.Body.Contains("Exception")) continue; //send to dead letter queue
+                    await client.DeleteMessageAsync("https://sqs.eu-central-1.amazonaws.com/075206908135/PostTimelineQueue", message.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
